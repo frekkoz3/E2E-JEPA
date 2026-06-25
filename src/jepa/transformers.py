dc46d8efb45179638b7c2f91673d09fcf23c3540
Capsia <cmsuser3754@zoho.eu>
2026-06-25 10:11:07 +0200

    No last layer norm param

--- src/jepa/transformers.py
+++ src/jepa/transformers.py
@@ -103,9 +103,10 @@ class Transformer(nn.Module):
         cond_dim=1,
         dropout=0.0,
         use_adaLN=False,
+        no_last_layer_norm=False
     ):
         super().__init__()
-        self.norm = nn.LayerNorm(hidden_dim)
+        self.norm = nn.LayerNorm(hidden_dim) if not no_last_layer_norm else nn.Identity()
         
         if input_dim != hidden_dim:
             self.input_proj = nn.Linear(input_dim, hidden_dim)
@@ -140,11 +141,11 @@ class Transformer(nn.Module):
 class VisualTransformer(nn.Module):
     """Visual encoder with patch embedding and transformer backbone."""
 
-    def __init__(self, img_size, embed_dim, mlp_dim, patch_size=16, num_heads=8, depth=6):
+    def __init__(self, img_size, embed_dim, mlp_dim, patch_size=16, num_heads=8, depth=6, no_last_layer_norm=False):
         super().__init__()
         self.patch_embed = PatchEmbedding(in_channels=3, embed_dim=embed_dim, patch_size=patch_size)
         self.pos_embed = PositionalEncoding(embed_dim=embed_dim, seq_len=(img_size[0]//patch_size)*(img_size[1]//patch_size))
-        self.transformer = Transformer(input_dim=embed_dim, hidden_dim=embed_dim, output_dim=embed_dim, depth=depth, num_heads=num_heads, mlp_dim=mlp_dim)
+        self.transformer = Transformer(input_dim=embed_dim, hidden_dim=embed_dim, output_dim=embed_dim, depth=depth, num_heads=num_heads, mlp_dim=mlp_dim, no_last_layer_norm=no_last_layer_norm)
         self.cls_token = nn.Parameter(torch.randn(1, 1, embed_dim))
 
 
